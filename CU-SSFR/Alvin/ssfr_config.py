3ef78e5b42f9ed90e3b9f3d30d841a413dd4ef3a
Hong Chen <me@hongchen.cz>
2018-07-11 15:44:14 -0600

    daily commit (too lazy to type)

--- CU-SSFR/Alvin/ssfr_config.py
+++ CU-SSFR/Alvin/ssfr_config.py
@@ -1,18 +1,18 @@
 config = {
-          'fname_primary_zen_cal'   : 'SSFR_data/Calibrations/Alvin/20180320/1324/zenith/SSIM1/si45_90i250_400/cal/20180320_spc00001.SKS',
-          'fname_primary_zen_dark'  : 'SSFR_data/Calibrations/Alvin/20180320/1324/zenith/SSIM1/si45_90i250_400/dark/20180320_spc00001.SKS',
-          'fname_primary_nad_cal'   : 'SSFR_data/Calibrations/Alvin/20180320/1324/nadir/2008-4/si45_90i250_400/cal/20180320_spc00001.SKS',
-          'fname_primary_nad_dark'  : 'SSFR_data/Calibrations/Alvin/20180320/1324/nadir/2008-4/si45_90i250_400/dark/20180320_spc00001.SKS',
+          'fname_primary_zen_cal'   : '/Users/hoch4240/Chen/work/00_reuse/SSFR-util/CU-SSFR/Alvin/data/post_cals/20180711/Alvin/1324/zenith/LCN-A-02/SSIM1/s45_90i250_375/cal/20180711_spc00001.SKS',
+          'fname_primary_zen_dark'  : '/Users/hoch4240/Chen/work/00_reuse/SSFR-util/CU-SSFR/Alvin/data/post_cals/20180711/Alvin/1324/zenith/LCN-A-02/SSIM1/s45_90i250_375/dark/20180711_spc00001.SKS',
+          'fname_primary_nad_cal'   : None,
+          'fname_primary_nad_dark'  : None,
 
-          'fname_transfer_zen_cal'  : 'SSFR_data/Calibrations/Alvin/20180320/150C/zenith/SSIM1/si45_90i250_400/cal/20180320_spc00001.SKS',
-          'fname_transfer_zen_dark' : 'SSFR_data/Calibrations/Alvin/20180320/150C/zenith/SSIM1/si45_90i250_400/dark/20180320_spc00001.SKS',
-          'fname_transfer_nad_cal'  : 'SSFR_data/Calibrations/Alvin/20180320/150C/nadir/2008-4/si45_90i250_400/cal/20180320_spc00001.SKS',
-          'fname_transfer_nad_dark' : 'SSFR_data/Calibrations/Alvin/20180320/150C/nadir/2008-4/si45_90i250_400/dark/20180320_spc00001.SKS',
+          'fname_transfer_zen_cal'  : '/Users/hoch4240/Chen/work/00_reuse/SSFR-util/CU-SSFR/Alvin/data/post_cals/20180711/Alvin/150C/zenith/LCN-A-02/SSIM1/s45_90i250_375/cal/20180711_spc00001.SKS',
+          'fname_transfer_zen_dark' : '/Users/hoch4240/Chen/work/00_reuse/SSFR-util/CU-SSFR/Alvin/data/post_cals/20180711/Alvin/150C/zenith/LCN-A-02/SSIM1/s45_90i250_375/dark/20180711_spc00001.SKS',
+          'fname_transfer_nad_cal'  : None,
+          'fname_transfer_nad_dark' : None,
 
-          'fname_secondary_zen_cal' : 'SSFR_data/Calibrations/Alvin/20180501/150C/zenith/SSIM/s45_90i250_375/cal/20180430_spc00001.SKS',
-          'fname_secondary_zen_dark': 'SSFR_data/Calibrations/Alvin/20180501/150C/zenith/SSIM/s45_90i250_375/dark/20180430_spc00001.SKS',
-          'fname_secondary_nad_cal' : 'SSFR_data/Calibrations/Alvin/20180501/150C/nadir/2008-4/s45_90i250_375/cal/20180430_spc00001.SKS',
-          'fname_secondary_nad_dark': 'SSFR_data/Calibrations/Alvin/20180501/150C/nadir/2008-4/s45_90i250_375/dark/20180430_spc00001.SKS'
+          'fname_secondary_zen_cal' : None,
+          'fname_secondary_zen_dark': None,
+          'fname_secondary_nad_cal' : None,
+          'fname_secondary_nad_dark': None
          }
 
 
