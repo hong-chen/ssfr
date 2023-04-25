a8502fcb979bbdeee3028544862ab357031642e1
hong-chen <hong.chen.cu@gmail.com>
2023-04-25 14:41:34 -0600

    minor changes

--- ssfr/common.py
+++ ssfr/common.py
@@ -5,9 +5,9 @@ __all__ = ['fdir_data']
 fdir_data = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 
 input_list = {
-        'which_ssfr': 'nasa',
+        'which_ssfr': 'lasp',
+          'which_lc': 'zenith',
          'wvl_joint': 950.0,
-         'wvl_start': 350.0,
-           'wvl_end': 2200.0,
+         'wvl_range': [350.0, 2200.0],
           'int_time': {'si':60, 'in':300},
         }
