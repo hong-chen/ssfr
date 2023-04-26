aabf8daafe49f34aa893277149560bffe4a0c09e
hong-chen <hong.chen.cu@gmail.com>
2023-04-26 14:22:28 -0600

    minor changes

--- tests/test_lasp-ssfr.py
+++ tests/test_lasp-ssfr.py
@@ -27,29 +27,10 @@ def test_process_lasp_ssfr():
     fdir = 'data/20221208_ssfr-lasp_pri-cal'
 
     fnames = ssfr.util.get_all_files(fdir)
-    print(fnames)
-    sys.exit()
 
-    fnames0 = {
-            'dark':'data/20221208_ssfr-lasp_pri-cal/20221208_CALIBRATION_75_150/20221208_spc00001.SKS',\
-            'cal' :'data/20221208_ssfr-lasp_pri-cal/20221208_CALIBRATION_75_150/20221208_spc00002.SKS',
-            }
-
-    fnames1 = {
-            'dark':'data/20221208_ssfr-lasp_pri-cal/20221208_CALIBRATION_250_500/20221208_spc00001.SKS',\
-            'cal' :'data/20221208_ssfr-lasp_pri-cal/20221208_CALIBRATION_250_500/20221208_spc00002.SKS',
-            }
-
-    ssfr.vis.quicklook_ssfr_raw(fnames0['dark'], extra_tag='Skywatch-test_075-150_')
-    ssfr.vis.quicklook_ssfr_raw(fnames0['cal'] , extra_tag='Skywatch-test_075-150_')
-    ssfr.vis.quicklook_ssfr_raw(fnames1['dark'], extra_tag='Skywatch-test_250-500_')
-    ssfr.vis.quicklook_ssfr_raw(fnames1['cal'] , extra_tag='Skywatch-test_250-500_')
-
-    data0_cal  = ssfr.lasp_ssfr.read_ssfr_raw(fnames0['cal'] , verbose=False)
-    data0_dark = ssfr.lasp_ssfr.read_ssfr_raw(fnames0['dark'], verbose=False)
-
-    data1_cal  = ssfr.lasp_ssfr.read_ssfr_raw(fnames1['cal'] , verbose=False)
-    data1_dark = ssfr.lasp_ssfr.read_ssfr_raw(fnames1['dark'], verbose=False)
+    for fname in fnames:
+        data0 = ssfr.lasp_ssfr.read_ssfr_raw(fname, verbose=False)
+        ssfr.vis.quicklook_ssfr_raw(fname, extra_tag='%s_' % fname.split('/')[-2])
 
 
 if __name__ == '__main__':
