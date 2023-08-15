3ec285dd1e6985f0ada1f268e8734468046f0a3e
hong-chen <hong.chen.cu@gmail.com>
2023-08-14 18:30:32 -0600

    minor changes

--- examples/04_magpie.py
+++ examples/04_magpie.py
@@ -284,14 +284,12 @@ if __name__ == '__main__':
             datetime.datetime(2023, 8, 13), \
         ]
 
-    for date in dates:
-    # for date in [dates[-1]]:
-        # cdata_magpie_hsk_v0(date)
-        # cdata_magpie_spns_v0(date)
-        cdata_magpie_spns_v1(date)
+    # for date in dates:
+    #     cdata_magpie_hsk_v0(date)
+    #     cdata_magpie_spns_v0(date)
+    #     cdata_magpie_spns_v1(date)
 
-    date_s = '2023-08-02'
-    # date_s = '2023-08-05'
-    # date_s = '2023-08-13'
-    fname = '/data/hong/mygit/ssfr/examples/MAGPIE_SPN-S_%s_v1.h5' % date_s
-    quicklook_bokeh_spns(fname, wvl0=None, tmhr0=None, tmhr_range=None, wvl_range=[350.0, 800.0], tmhr_step=10, wvl_step=2, description='MAGPIE', fname_html='spns-ql_magpie_%s.html' % date_s)
+    for date in dates:
+        date_s = date.strftime('%Y-%m-%d')
+        fname = 'MAGPIE_SPN-S_%s_v1.h5' % date_s
+        ssfr.vis.quicklook_bokeh_spns(fname, wvl0=None, tmhr0=None, tmhr_range=None, wvl_range=[350.0, 800.0], tmhr_step=10, wvl_step=2, description='MAGPIE', fname_html='spns-ql_magpie_%s.html' % date_s)
