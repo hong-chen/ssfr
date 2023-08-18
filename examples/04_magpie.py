75b5a193c2f78f2831ac26d32be9254e603fea7b
hong-chen <hong.chen.cu@gmail.com>
2023-08-18 15:22:19 -0600

    minor changes

--- examples/04_magpie.py
+++ examples/04_magpie.py
@@ -23,80 +23,6 @@ _fdir_data_ = '/argus/field'
 _fdir_v0_  = '/argus/field/magpie/2023/dhc6/processed'
 _fdir_v1_  = '/argus/field/magpie/2023/dhc6/processed'
 
-def preview_magpie(fdir):
-
-    fname_dif = sorted(glob.glob('%s/Diffuse.txt' % fdir))[0]
-    data0_dif = ssfr.lasp_spn.read_spns(fname=fname_dif)
-
-    fname_tot = sorted(glob.glob('%s/Total.txt' % fdir))[0]
-    data0_tot = ssfr.lasp_spn.read_spns(fname=fname_tot)
-
-    wvl0 = 555.0
-
-    iwvl0_dif = np.argmin(np.abs(wvl0-data0_dif.data['wavelength']))
-    iwvl0_tot = np.argmin(np.abs(wvl0-data0_tot.data['wavelength']))
-
-    # figure
-    #/----------------------------------------------------------------------------\#
-    if True:
-        plt.close('all')
-        fig = plt.figure(figsize=(12, 10))
-        fig.suptitle('SPN-S on %s (MAGPIE)' % os.path.basename(fdir))
-
-        # plot
-        #/--------------------------------------------------------------\#
-        ax1 = fig.add_subplot(211)
-        ax1.scatter(data0_dif.data['tmhr'], data0_dif.data['flux'][:, iwvl0_dif], s=3, c='gray', lw=0.0)
-        ax1.scatter(data0_tot.data['tmhr'], data0_tot.data['flux'][:, iwvl0_tot], s=3, c='black', lw=0.0)
-        ax1.set_xlabel('UTC Time [Hour]')
-        ax1.set_ylabel('Irradiance [$W m^{-2} nm^{-1}$]')
-        ax1.set_title('Time Series of Downwelling Irradiance at %d nm' % wvl0)
-        ax1.set_ylim(0.0)
-
-        patches_legend = [
-                          mpatches.Patch(color='black' , label='Total'), \
-                          mpatches.Patch(color='gray'  , label='Diffuse'), \
-                         ]
-        ax1.legend(handles=patches_legend, loc='upper right', fontsize=16)
-
-        percentiles = np.array([10, 50, 90])
-        colors = ['red', 'blue', 'green']
-        logic_valid = (data0_tot.data['tmhr']>=0.0) & (data0_tot.data['tmhr']<24.0) & (data0_tot.data['flux'][:, iwvl0_tot]>0.0) & (data0_tot.data['flux'][:, iwvl0_tot]<5.0)
-        selected_tmhr = np.percentile(data0_tot.data['tmhr'][logic_valid], percentiles)
-        for i in range(percentiles.size):
-            ax1.axvline(selected_tmhr[i], color=colors[i], lw=1.5, ls='--')
-        #\--------------------------------------------------------------/#
-
-        #
-        #/--------------------------------------------------------------\#
-        ax2 = fig.add_subplot(212)
-        for i in range(percentiles.size):
-            itmhr_dif = np.argmin(np.abs(data0_dif.data['tmhr'][logic_valid]-selected_tmhr[i]))
-            itmhr_tot = np.argmin(np.abs(data0_tot.data['tmhr'][logic_valid]-selected_tmhr[i]))
-            ax2.plot(data0_tot.data['wavelength'], data0_tot.data['flux'][logic_valid, ...][itmhr_tot, :], ls='-' , color=colors[i])
-            ax2.plot(data0_dif.data['wavelength'], data0_dif.data['flux'][logic_valid, ...][itmhr_dif, :], ls='--', color=colors[i])
-        ax2.axvline(wvl0, color='k', lw=1.5, ls='--')
-        ax2.set_xlabel('Wavelength [nm]')
-        ax2.set_ylabel('Irradiance [$W m^{-2} nm^{-1}$]')
-        ax2.set_title('Spectral Downwelling Irradiance')
-        ax2.set_xlim((350, 800))
-        ax2.set_ylim(0.0)
-
-        patches_legend = [
-                          mpatches.Patch(edgecolor='black', ls='-' , facecolor='None', label='Total'), \
-                          mpatches.Patch(edgecolor='black', ls='--', facecolor='None', label='Diffuse'), \
-                         ]
-        ax2.legend(handles=patches_legend, loc='upper right', fontsize=16)
-        #\--------------------------------------------------------------/#
-
-        # save figure
-        #/--------------------------------------------------------------\#
-        fig.subplots_adjust(hspace=0.4, wspace=0.3)
-        _metadata = {'Computer': os.uname()[1], 'Script': os.path.abspath(__file__), 'Function':sys._getframe().f_code.co_name, 'Date':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-        fig.savefig('%s_%s.png' % (_metadata['Function'], os.path.basename(fdir)), bbox_inches='tight', metadata=_metadata)
-        #\--------------------------------------------------------------/#
-        plt.show()
-    #\----------------------------------------------------------------------------/#
 
 
 def cdata_magpie_hsk_v0(
@@ -271,6 +197,32 @@ def cdata_magpie_spns_v1(
     f.close()
     #\----------------------------------------------------------------------------/#
 
+def cdata_sat_img(
+        date,
+        margin=0.1,
+        fdir_data=_fdir_v0_,
+        ):
+
+    # read hsk v0
+    #/----------------------------------------------------------------------------\#
+    fname_h5 = '%s/MAGPIE_HSK_%s_v0.h5' % (fdir_data, date.strftime('%Y-%m-%d'))
+    f = h5py.File(fname_h5, 'r')
+    jday = f['jday'][...]
+    sza  = f['sza'][...]
+    tmhr = f['tmhr'][...]
+    lon  = f['lon'][...]
+    lat  = f['lat'][...]
+    alt  = f['alt'][...]
+    f.close()
+    #\----------------------------------------------------------------------------/#
+
+    # extent = [np.nanmin(lon)-margin, np.nanmax(lon)+margin, np.nanmin(lat)-margin, np.nanmax(lat)+margin]
+    extent = [-60.5, -58.5, 12, 14]
+
+    command = 'sdown --date %s --extent %s --products MODRGB MYDRGB VNPRGB VJ1RGB --fdir data/sat-img' % (date.strftime('%Y%m%d'), ' '.join([str(a) for a in extent]))
+
+    print(command)
+
 
 if __name__ == '__main__':
 
@@ -279,6 +231,8 @@ if __name__ == '__main__':
             datetime.datetime(2023, 8, 3),  \
             datetime.datetime(2023, 8, 5),  \
             datetime.datetime(2023, 8, 13), \
+            datetime.datetime(2023, 8, 14), \
+            datetime.datetime(2023, 8, 15), \
         ]
 
     for date in dates:
@@ -290,3 +244,5 @@ if __name__ == '__main__':
         date_s = date.strftime('%Y-%m-%d')
         fname = '%s/MAGPIE_SPN-S_%s_v1.h5' % (_fdir_v1_, date_s)
         ssfr.vis.quicklook_bokeh_spns(fname, wvl0=None, tmhr0=None, tmhr_range=None, wvl_range=[350.0, 800.0], tmhr_step=10, wvl_step=5, description='MAGPIE', fname_html='spns-ql_magpie_%s.html' % date_s)
+
+    cdata_sat_img(dates[-1])
