5174c1c84fea3f4ad5936b7ba3a8a189a7ab00aa
hong-chen <hong.chen.cu@gmail.com>
2023-08-26 20:45:37 -0600

    minor changes

--- examples/04_magpie.py
+++ examples/04_magpie.py
@@ -501,7 +501,8 @@ if __name__ == '__main__':
             # datetime.datetime(2023, 8, 21),
             # datetime.datetime(2023, 8, 22),
             # datetime.datetime(2023, 8, 23),
-            datetime.datetime(2023, 8, 25),
+            # datetime.datetime(2023, 8, 25),
+            datetime.datetime(2023, 8, 26),
         ]
 
     for date in dates:
