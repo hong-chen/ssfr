c55abc95f2b9e7a7b6657d7416f0635caddb2f7b
Hong Chen <hong.chen.cu@gmail.com>
2023-11-13 15:37:02 -0700

    minor changes

---
+++ ssfr/cal/wvl_cal.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import glob
+import datetime
+
+def poly_fit():
+
+    pass
--- ssfr/common.py
+++ ssfr/common.py
@@ -11,3 +11,18 @@ input_list = {
          'wvl_range': [350.0, 2200.0],
           'int_time': {'si':60, 'in':300},
         }
+
+serial_number = {
+        'lasp|ssfr-a|zenith|silicon': 'n/a',
+        'lasp|ssfr-a|zenith|ingaas' : '044832', # found broken and replaced with Zenith InGaAs spectrometer from NASA Ames's SSFR
+        'lasp|ssfr-a|nadir|silicon' : 'n/a',
+        'lasp|ssfr-a|nadir|ingaas'  : 'n/a',
+        'lasp|ssfr-b|zenith|silicon': 'n/a',
+        'lasp|ssfr-b|zenith|ingaas' : 'n/a',
+        'lasp|ssfr-b|nadir|silicon' : 'n/a',
+        'lasp|ssfr-b|nadir|ingaas'  : 'n/a',
+        'nasa|ssfr|zenith|silicon': '033161',
+        'nasa|ssfr|zenith|ingaas' : '044832',
+        'nasa|ssfr|nadir|silicon' : '045924',
+        'nasa|ssfr|nadir|ingaas'  : '044829',
+        }
