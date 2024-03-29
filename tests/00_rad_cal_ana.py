ab8a4d6b64319df4052ba0c0bd3b7698f8ae6a4a
Hong Chen <hong.chen.cu@gmail.com>
2024-03-29 10:54:03 -0600

    minor changes

--- tests/00_rad_cal_ana.py
+++ tests/00_rad_cal_ana.py
@@ -114,46 +114,83 @@ def main_calibration():
     #\----------------------------------------------------------------------------/#
 
 
+def field_lamp_150c_consis_check():
 
-def field_lamp_consis_check():
+    fnames = sorted(glob.glob('*150c*si-080*.h5'))
 
-    fnames = sorted(glob.glob('*150e*.h5'))
+    colors = plt.cm.jet(np.linspace(0.0, 1.0, len(fnames)))
 
     # figure
     #/----------------------------------------------------------------------------\#
     if True:
         plt.close('all')
         fig = plt.figure(figsize=(8, 6))
-        # fig.suptitle('Figure')
-        # plot
-        #/--------------------------------------------------------------\#
+        fig.suptitle('Field Lamp 150c', fontsize=24)
+
         ax1 = fig.add_subplot(111)
 
-        for fname in fnames:
+        for i, fname in enumerate(fnames):
+            tags = os.path.basename(fname).replace('.h5', '').split('|')
             f = h5py.File(fname, 'r')
             wvl = f['wvl'][...]
-            transfer = f['pri_resp'][...]
+            transfer = f['transfer'][...]
             f.close()
-        # cs = ax1.imshow(.T, origin='lower', cmap='jet', zorder=0) #, extent=extent, vmin=0.0, vmax=0.5)
-            ax1.scatter(wvl, transfer, s=6, lw=0.0)
-        # ax1.hist(.ravel(), bins=100, histtype='stepfilled', alpha=0.5, color='black')
-        # ax1.plot([0, 1], [0, 1], color='k', ls='--')
-        # ax1.set_xlim(())
-        # ax1.set_ylim(())
-        # ax1.set_xlabel('')
-        # ax1.set_ylabel('')
-        # ax1.set_title('')
-        # ax1.xaxis.set_major_locator(FixedLocator(np.arange(0, 100, 5)))
-        # ax1.yaxis.set_major_locator(FixedLocator(np.arange(0, 100, 5)))
-        #\--------------------------------------------------------------/#
+
+            ax1.scatter(wvl, transfer, s=6, lw=0.0, color=colors[i, ...], label='%s|%s|%s|%s' % (tags[7], tags[8], tags[1], tags[0]))
+
+        ax1.set_xlabel('Wavelength [nm]')
+        ax1.set_ylabel('Irradiance [$W m^{-2} nm^{-1}$]')
+        ax1.set_xlim((350, 2150))
+        ax1.set_ylim((0, 0.35))
+        plt.legend(fontsize=10)
+
+
         # save figure
         #/--------------------------------------------------------------\#
-        # fig.subplots_adjust(hspace=0.3, wspace=0.3)
-        # _metadata = {'Computer': os.uname()[1], 'Script': os.path.abspath(__file__), 'Function':sys._getframe().f_code.co_name, 'Date':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-        # fig.savefig('%s.png' % _metadata['Function'], bbox_inches='tight', metadata=_metadata)
+        fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        _metadata = {'Computer': os.uname()[1], 'Script': os.path.abspath(__file__), 'Function':sys._getframe().f_code.co_name, 'Date':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+        fig.savefig('%s.png' % (_metadata['Function']), bbox_inches='tight', metadata=_metadata)
         #\--------------------------------------------------------------/#
-        plt.show()
-        sys.exit()
+    #\----------------------------------------------------------------------------/#
+
+
+def field_lamp_150e_consis_check():
+
+    fnames = sorted(glob.glob('*150e*si-080*.h5'))
+
+    colors = plt.cm.jet(np.linspace(0.0, 1.0, len(fnames)))
+
+    # figure
+    #/----------------------------------------------------------------------------\#
+    if True:
+        plt.close('all')
+        fig = plt.figure(figsize=(8, 6))
+        fig.suptitle('Field Lamp 150e', fontsize=24)
+
+        ax1 = fig.add_subplot(111)
+
+        for i, fname in enumerate(fnames):
+            tags = os.path.basename(fname).replace('.h5', '').split('|')
+            f = h5py.File(fname, 'r')
+            wvl = f['wvl'][...]
+            transfer = f['transfer'][...]
+            f.close()
+
+            ax1.scatter(wvl, transfer, s=6, lw=0.0, color=colors[i, ...], label='%s|%s|%s|%s' % (tags[7], tags[8], tags[1], tags[0]))
+
+        ax1.set_xlabel('Wavelength [nm]')
+        ax1.set_ylabel('Irradiance [$W m^{-2} nm^{-1}$]')
+        ax1.set_xlim((350, 2150))
+        ax1.set_ylim((0, 0.35))
+        plt.legend(fontsize=10)
+
+
+        # save figure
+        #/--------------------------------------------------------------\#
+        fig.subplots_adjust(hspace=0.3, wspace=0.3)
+        _metadata = {'Computer': os.uname()[1], 'Script': os.path.abspath(__file__), 'Function':sys._getframe().f_code.co_name, 'Date':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+        fig.savefig('%s.png' % (_metadata['Function']), bbox_inches='tight', metadata=_metadata)
+        #\--------------------------------------------------------------/#
     #\----------------------------------------------------------------------------/#
 
 
@@ -165,8 +202,11 @@ if __name__ == '__main__':
     # for fname in [fnames[-1]]:
     #     fig_cos_resp(fname)
 
-    main_calibration()
+    # main_calibration()
 
-    # field_lamp_consis_check()
+    # for fname in fnames:
+    #     field_lamp_consis_check(fname)
 
+    # field_lamp_150c_consis_check()
+    field_lamp_150e_consis_check()
     pass
