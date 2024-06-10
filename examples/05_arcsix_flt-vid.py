828a67253b6e53130ec9eaf5212128e971c01154
Hong Chen <hong.chen.cu@gmail.com>
2024-06-10 13:15:21 -0600

    minor changes

--- examples/05_arcsix_flt-vid.py
+++ examples/05_arcsix_flt-vid.py
@@ -2390,7 +2390,7 @@ def plot_video_frame_arcsix(statements, test=False):
     # acknowledgements
     #/----------------------------------------------------------------------------\#
     text1 = '\
-presented by ARCSIX SSFR Team - H. Chen, V. Nataraja, A. Chamberlain, J. Drouet, Y.-W. Chen, K. Hirata, K. Dong, and S. Schmidt | MetNav - R. Bennett | MARLi - Z. Wang\n\
+presented by ARCSIX SSFR Team - H. Chen, V. Nataraja, J. Drouet, A. Chamberlain, Y.-W. Chen, K. Hirata, K. Dong, and S. Schmidt | MetNav - R. Bennett | MARLi - Z. Wang\n\
 '
     ax.annotate(text1, xy=(0.5, 0.26), fontsize=8, color='gray', xycoords='axes fraction', ha='center', va='center')
 
@@ -2800,14 +2800,14 @@ if __name__ == '__main__':
             # datetime.datetime(2024, 5, 17), # ARCSIX test flight #1 near NASA WFF
             # datetime.datetime(2024, 5, 21), # ARCSIX test flight #2 near NASA WFF
             # datetime.datetime(2024, 5, 24), # ARCSIX transit flight #1 from NASA WFF to Pituffik Space Base
-            # datetime.datetime(2024, 5, 28), # ARCSIX science flight #1; clear-sky spiral
+            datetime.datetime(2024, 5, 28), # ARCSIX science flight #1; clear-sky spiral
             # datetime.datetime(2024, 5, 30), # ARCSIX science flight #2; cloud wall
             # datetime.datetime(2024, 5, 31), # ARCSIX science flight #3; bowling alley, surface BRDF
             # datetime.datetime(2024, 6, 3), # ARCSIX science flight #4; cloud wall, (No MARLi)
             # datetime.datetime(2024, 6, 5), # ARCSIX science flight #5; bowling alley, surface BRDF (MARLi NaN)
             # datetime.datetime(2024, 6, 6), # ARCSIX science flight #6; cloud wall
             # datetime.datetime(2024, 6, 7), # ARCSIX science flight #7; cloud wall (No MARLi)
-            datetime.datetime(2024, 6, 10), # ARCSIX science flight #8; cloud wall (No MARLi, No MetNav)
+            # datetime.datetime(2024, 6, 10), # ARCSIX science flight #8; cloud wall (No MARLi, No MetNav)
         ]
 
     for date in dates[::-1]:
