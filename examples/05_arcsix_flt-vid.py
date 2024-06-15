d90267a9a3a634766362ab62dd3aa99cc0a135dd
Hong Chen <hong.chen.cu@gmail.com>
2024-06-14 21:46:41 -0600

    minor changes

--- examples/05_arcsix_flt-vid.py
+++ examples/05_arcsix_flt-vid.py
@@ -2826,12 +2826,12 @@ if __name__ == '__main__':
             # datetime.datetime(2024, 5, 30), # [✓] ARCSIX science flight #2; cloud wall
             # datetime.datetime(2024, 5, 31), # [✓] ARCSIX science flight #3; bowling alley, surface BRDF
             # datetime.datetime(2024, 6,  3), # ARCSIX science flight #4; cloud wall, (no MARLi)
-            # datetime.datetime(2024, 6,  5), # ARCSIX science flight #5; bowling alley, surface BRDF (no camera)
+            datetime.datetime(2024, 6,  5), # ARCSIX science flight #5; bowling alley, surface BRDF (no camera)
             # datetime.datetime(2024, 6,  6), # ARCSIX science flight #6; cloud wall (no camera)
             # datetime.datetime(2024, 6,  7), # ARCSIX science flight #7; cloud wall (no camera)
             # datetime.datetime(2024, 6, 10), # ARCSIX science flight #8; cloud wall (no camera)
             # datetime.datetime(2024, 6, 11), #  [✓] ARCSIX science flight #9; cloud wall
-            datetime.datetime(2024, 6, 13), # ARCSIX science flight #10 (no camera)
+            # datetime.datetime(2024, 6, 13), # ARCSIX science flight #10 (no camera)
         ]
 
     for date in dates[::-1]:
@@ -2849,7 +2849,7 @@ if __name__ == '__main__':
             main_pre_arcsix(date)
             main_vid_arcsix(date, wvl0=_wavelength_, interval=60) # make quickview video
             main_vid_arcsix(date, wvl0=_wavelength_, interval=20) # make sharable video
-            main_vid_arcsix(date, wvl0=_wavelength_, interval=5)  # make complete video
+            # main_vid_arcsix(date, wvl0=_wavelength_, interval=5)  # make complete video
             #\----------------------------------------------------------------------------/#
             pass
 
