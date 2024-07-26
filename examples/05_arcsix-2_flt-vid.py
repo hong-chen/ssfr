8116ae5df9eba3bbc41f9c5f19b200a0da5cd14a
Hong Chen <hong.chen.cu@gmail.com>
2024-07-26 15:11:06 -0600

    minor changes

--- examples/05_arcsix-2_flt-vid.py
+++ examples/05_arcsix-2_flt-vid.py
@@ -1773,18 +1773,18 @@ def main_vid_arcsix(
 if __name__ == '__main__':
 
     dates = [
-            # datetime.datetime(2024, 5, 28), # [✓] ARCSIX science flight #1; clear-sky spiral
-            # datetime.datetime(2024, 5, 30), # [✓] ARCSIX science flight #2; cloud wall
-            # datetime.datetime(2024, 5, 31), # [✓] ARCSIX science flight #3; bowling alley, surface BRDF
-            # datetime.datetime(2024, 6,  3), # ARCSIX science flight #4; cloud wall, (no MARLi)
-            # datetime.datetime(2024, 6,  5), # [✓] ARCSIX science flight #5; bowling alley, surface BRDF
-            # datetime.datetime(2024, 6,  6), # [✓] ARCSIX science flight #6; cloud wall
-            # datetime.datetime(2024, 6,  7), # [✓] ARCSIX science flight #7; cloud wall
-            # datetime.datetime(2024, 6, 10), # [✓] ARCSIX science flight #8; cloud wall
-            # datetime.datetime(2024, 6, 11), # [✓] ARCSIX science flight #9; cloud wall
-            # datetime.datetime(2024, 6, 13), # [✓] ARCSIX science flight #10
+            datetime.datetime(2024, 5, 28), # [✓] ARCSIX science flight #1; clear-sky spiral
+            datetime.datetime(2024, 5, 30), # [✓] ARCSIX science flight #2; cloud wall
+            datetime.datetime(2024, 5, 31), # [✓] ARCSIX science flight #3; bowling alley, surface BRDF
+            datetime.datetime(2024, 6,  3), # ARCSIX science flight #4; cloud wall, (no MARLi)
+            datetime.datetime(2024, 6,  5), # [✓] ARCSIX science flight #5; bowling alley, surface BRDF
+            datetime.datetime(2024, 6,  6), # [✓] ARCSIX science flight #6; cloud wall
+            datetime.datetime(2024, 6,  7), # [✓] ARCSIX science flight #7; cloud wall
+            datetime.datetime(2024, 6, 10), # [✓] ARCSIX science flight #8; cloud wall
+            datetime.datetime(2024, 6, 11), # [✓] ARCSIX science flight #9; cloud wall
+            datetime.datetime(2024, 6, 13), # [✓] ARCSIX science flight #10
             # datetime.datetime(2024, 7, 22), # [✓] ARCSIX transit flight #3
-            datetime.datetime(2024, 7, 25), # [✓] ARCSIX science flight # 11; cloud wall
+            # datetime.datetime(2024, 7, 25), # [✓] ARCSIX science flight # 11; cloud wall
         ]
 
     for date in dates[::-1]:
@@ -1793,9 +1793,9 @@ if __name__ == '__main__':
         #/----------------------------------------------------------------------------\#
         # post_process_sat_img_vn(date)
         main_pre_arcsix(date)
-        main_vid_arcsix(date, wvl0=_wavelength_, interval=60) # make quickview video
+        # main_vid_arcsix(date, wvl0=_wavelength_, interval=60) # make quickview video
         # main_vid_arcsix(date, wvl0=_wavelength_, interval=20) # make sharable video
-        # main_vid_arcsix(date, wvl0=_wavelength_, interval=5)  # make complete video
+        main_vid_arcsix(date, wvl0=_wavelength_, interval=5)  # make complete video
         #\----------------------------------------------------------------------------/#
         pass
 
