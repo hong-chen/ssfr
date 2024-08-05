00301a08dadf5ee77d5fb86001acf5961a8812e5
Hong Chen <hong.chen.cu@gmail.com>
2024-08-05 12:21:06 -0600

    minor changes

--- examples/05_arcsix-2_flt-vid.py
+++ examples/05_arcsix-2_flt-vid.py
@@ -1823,8 +1823,8 @@ if __name__ == '__main__':
             # datetime.datetime(2024, 7, 25), # [✓] ARCSIX science flight # 11; cloud wall
             # datetime.datetime(2024, 7, 29), # [✓] ARCSIX science flight # 12; clear-sky BRDF
             # datetime.datetime(2024, 7, 30), # [✓] ARCSIX science flight # 13; clear-sky BRDF
-            # datetime.datetime(2024, 8, 1), # [✓] ARCSIX science flight # 14; cloud walls
-            datetime.datetime(2024, 8, 2), # [✓] ARCSIX science flight # 14; cloud walls
+            datetime.datetime(2024, 8, 1), # [✓] ARCSIX science flight # 14; cloud walls
+            # datetime.datetime(2024, 8, 2), # [✓] ARCSIX science flight # 14; cloud walls
         ]
 
     for date in dates[::-1]:
